df4b76f914a0ea78d5856fc80430dad387eab2d2
sandunsameera <sandunsameera25@gmail.com>
2019-03-02 20:19:22 +0530

    Identify fragment ui update

--- app/src/main/java/com/lolin/deemon_face/quito/IdentifyFragment.java
+++ app/src/main/java/com/lolin/deemon_face/quito/IdentifyFragment.java
@@ -6,12 +6,15 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 
 /**
  * A simple {@link Fragment} subclass.
  */
 public class IdentifyFragment extends Fragment {
+
+    Button imageButton;
 
 
     public IdentifyFragment() {
@@ -23,7 +26,10 @@ public class IdentifyFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
+
+
         return inflater.inflate (R.layout.fragment_identify, container, false);
+
     }
 
 
