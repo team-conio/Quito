2876773be0e9b6dd45803c5e3f18fb8565a589d4
sandunsameera <sandunsameera25@gmail.com>
2019-03-04 20:03:17 +0530

    List View in Tips fragment

--- app/src/main/java/com/lolin/deemon_face/quito/TipsFragment.java
+++ app/src/main/java/com/lolin/deemon_face/quito/TipsFragment.java
@@ -6,6 +6,8 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 
 /**
@@ -22,8 +24,25 @@ public class TipsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        View view =inflater.inflate (R.layout.fragment_tips, container, false);
+
+        String[] Tips = {"First Tip",
+                            "Second Tip",
+                            "Third tip"};
+
+        ListView listView = view.findViewById (R.id.Tips_listView);
+
+        ArrayAdapter<String> listViewAdapter = new ArrayAdapter<String> (
+                getActivity (),
+                android.R.layout.simple_list_item_1,
+                Tips
+
+        );
+
+        listView.setAdapter (listViewAdapter);
         // Inflate the layout for this fragment
-        return inflater.inflate (R.layout.fragment_tips, container, false);
+        return view;
     }
 
 }
